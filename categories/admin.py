1d89cabfcb6a5371359ff5f0614243ab04e90ad6
Nerd <studytime023@gmail.com>
2023-10-28 01:39:57 +0300

    refractor (Admin) : set fields and save_model

--- categories/admin.py
+++ categories/admin.py
@@ -2,6 +2,11 @@ from .models import Category
 from django.contrib import admin
 
 class CategoryAdmin(admin.ModelAdmin):
+    fields=["title","description"]
     list_display=["title","pk","description","author"]
+
+    def save_model(self,request,obj,form,change):
+        obj.author=request.user
+        super().save_model(request,obj,form,change)
 
 admin.site.register(Category,CategoryAdmin)
