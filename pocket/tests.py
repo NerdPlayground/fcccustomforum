483f026e935c9e1f9367ef514f26b5b79264932a
Nerd <studytime023@gmail.com>
2023-10-27 18:40:43 +0300

    refractor (Tests) : create global login method

--- pocket/tests.py
+++ pocket/tests.py
@@ -2,6 +2,13 @@ from django.urls import reverse
 from django.test import TestCase
 
 class PocketTestCase(TestCase):
+    def member_login(self,member,password):
+        response=self.client.post(reverse("login"),{
+            "username":member.username,
+            "password":password,
+        })
+        self.assertEqual(response.status_code,302)
+
     def url_template(self,url,template,title,kwargs=None,relativeURL=False):
         response=self.client.get(
             url if relativeURL 
